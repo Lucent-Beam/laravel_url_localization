df4ecf2d29452b7214c2ed202c5e874c4f4bb8f9
Luis Charres <luis.charres.06@gmail.com>
2016-02-22 12:35:46 -0500

    First commit

---
+++ app/views/localization.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Multi</title>
+  </head>
+  <body>
+
+    <h1>{{ trans('localization_test.title') }}</h1>
+    <h2>{{ trans('localization_test.subtitle') }}</h2>
+
+
+  </body>
+</html>
